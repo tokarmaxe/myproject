c53e18cb857664503ce73837a18adad0dff241c2
Maxim Tokar <tokarmaxe@gmail.com>
2018-01-08 19:02:40 +0200

    add comments in script.js, make more good visual css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-$('#asd').click(function (e) {
+// ф-я, которая отменяет стандартные настройки браузера и замена атребута по клику на изображение
+// и делает видимым lightbox.
+$('a').click(function (e) {
     e.preventDefault();
-
-    $('.lightbox').find('img').attr('src',$(this).attr('href'));
+    $('.image').attr("src",$(this).attr('href'));
     $('.lightbox').fadeIn();
+});
+// ф-я, закрывающая lightbox по клику на крестик
+$('.closediv').click(function () {
+    $('.lightbox').fadeOut();
 });
