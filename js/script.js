f75f5d59956adcd8464cee303039e0632c8c991a
Maxim Tokar <tokarmaxe@gmail.com>
2018-01-08 19:59:17 +0200

    last version of my simple gallery

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 // и делает видимым lightbox.
 $('a').click(function (e) {
     e.preventDefault();
+    $('.mygallery').css('filter','blur(5px)');
     $('.image').attr("src",$(this).attr('href'));
     $('.lightbox').fadeIn();
 });
 // ф-я, закрывающая lightbox по клику на крестик
 $('.closediv').click(function () {
+    $('.mygallery').css('filter','blur(0px)');
     $('.lightbox').fadeOut();
 });
